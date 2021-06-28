300479d6ef4fd7eafd66953f774d5171c046c8cb
Lee Sumin <sumini0715@naver.com>
2021-06-28 14:46:48 +0900

    put explanation on ConflictListener

--- app/src/main/java/com/example/shootinggame/ConflictListener.java
+++ app/src/main/java/com/example/shootinggame/ConflictListener.java
@@ -1,5 +1,8 @@
 package com.example.shootinggame;
 
+/**
+ * Board에서 Bullet과 Enemy의 충돌을 감지하여 MainActivity로 알려주기 위한 Listener
+ */
 public interface ConflictListener {
     public void conflict(Enemy e, Bullet b);
 }
